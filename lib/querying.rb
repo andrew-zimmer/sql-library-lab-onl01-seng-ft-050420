3421e25fa603493d9c000ac58727848dbfe5454f
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-06-02 22:15:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
-  INNER JOIN series ON books.series_id = series_id
-  
-  ORDER BY books.year;"
+  INNER JOIN series ON books.series_id = series_id;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
